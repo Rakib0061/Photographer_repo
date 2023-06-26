30a5e0a3b8a2e99ad9dd93749902011e3905da72
Rakib0061 <rakibulislam0061@gmail.com>
2023-06-27 00:03:44 +0600

    done almost exclusive part

--- portfolio/app.js
+++ portfolio/app.js
@@ -14,11 +14,36 @@ window.addEventListener("scroll", () => {
 const cv_btn_p = document.getElementById("cv_btn_p");
 const cv_btn = document.getElementById("cv_btn");
 
-cv_btn_p.innerHTML = cv_btn_p.innerText.split("").map((vlu, i) =>
-  `<span style = "transform: rotate(${i * 10}deg);position: absolute;transform-origin: 0px 65px;">${vlu}</span>`).join("");
+cv_btn_p.innerHTML = cv_btn_p.innerText
+  .split("")
+  .map(
+    (vlu, i) =>
+      `<span style = "transform: rotate(${
+        i * 10
+      }deg);position: absolute;transform-origin: 0px 65px;">${vlu}</span>`
+  )
+  .join("");
 
+//  @@@@@@@@@@@@@@@@@@@@@@@@@@@ Sticky Navbar @@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+//  @@@@@@@@@@@@@@@@@@@@@@@@@@@ Sticky Navbar @@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-/* */
+let exclusiveShot_item_2 = document.querySelectorAll(".exclusiveShot_item_2");
+
+document.addEventListener("scroll", () => {
+  let Y = window.scrollY;
+  
+  if (Y >= 1550) {
+    exclusiveShot_item_2.forEach((vlu) => {
+      vlu.classList.add("paralax");
+    });
+  }
+  else if (Y <= 1550) {
+    exclusiveShot_item_2.forEach((vlu) => {
+      vlu.classList.remove("paralax");
+    });
+  }
+
+});
 
 //  @@@@@@@@@@@@@@@@@@@@@@@@@@@ Sticky Navbar @@@@@@@@@@@@@@@@@@@@@@@@@@@
